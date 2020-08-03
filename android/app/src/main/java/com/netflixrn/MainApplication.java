4c15457259cf5de2420bba438400de0d204dc942
Jonathan Eduardo Meriño Bolivar <jmerino1204@gmail.com>
2020-08-03 17:28:51 -0500

    Ultimo funcional

--- android/app/src/main/java/com/netflixrn/MainApplication.java
+++ android/app/src/main/java/com/netflixrn/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.brentvatne.react.ReactVideoPackage;
 import com.brentvatne.react.ReactVideoPackage;
+import com.brentvatne.react.ReactVideoPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.facebook.react.ReactInstanceManager;
